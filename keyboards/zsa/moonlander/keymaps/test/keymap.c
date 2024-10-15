1e6a1d44cd3915b9305d4e50c96635f9efb9e737
bresch <brescianimathieu@gmail.com>
2024-10-15 09:22:05 +0200

    moonlander: initial canary config

---
+++ keyboards/zsa/moonlander/keymaps/test/keymap.c
@@ -0,0 +1,88 @@
+/* Copyright 2020 ZSA Technology Labs, Inc <@zsa>
+ * Copyright 2020 Jack Humbert <jack.humb@gmail.com>
+ * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#include QMK_KEYBOARD_H
+#include "version.h"
+
+enum layers {
+    BASE,
+    NUMB,
+    SYMB,
+    MOVE,
+};
+
+enum custom_keycodes {
+    VRSN = SAFE_RANGE,
+};
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [BASE] = LAYOUT(
+    _______, _______, _______, KC_QUES, KC_AUDIO_MUTE,  KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,  _______, _______, _______, KC_EXLM,KC_EQUAL, _______,KC_DELETE,
+    _______, KC_TAB , KC_L   , KC_Y   , KC_P   , KC_B   ,KC_GRAVE,                          _______, KC_Z   , KC_F   , KC_O   , KC_U   , _______, _______,
+    KC_ESC , KC_C   , KC_R   , KC_S   , KC_T   , KC_G   , CW_TOGG,                         KC_MINUS, KC_M   , KC_N   , KC_E   , KC_I   , KC_A   , KC_SCLN,
+    KC_Q   , KC_W   , KC_J   , KC_V   , KC_D   , KC_K   ,                                            KC_X   , KC_H   ,KC_QUOTE,KC_COMMA, KC_DOT , _______,
+    _______, _______, _______, _______, KC_UNDS,                   _______,            _______,               KC_ASTR, KC_LABK, KC_RABK, _______, _______,
+    KC_SPACE,OSL(NUMB),KC_BSLS,                                                                  KC_SLASH,OSL(SYMB),KC_BSPC
+  ),
+
+  [NUMB] = LAYOUT_moonlander(
+    _______, _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, OSL(MOVE),         _______, _______, _______,                                 _______, _______, KC_7,           KC_8,           KC_9,           _______, _______,
+    TO(0),          OSM(MOD_LGUI),  OSM(MOD_LALT),  OSM(MOD_LSFT),  OSM(MOD_LCTL),  _______, _______,                                                                 _______, _______, KC_4,           KC_5,           KC_6,           KC_PLUS,        _______,
+    _______, _______, _______, _______, _______, _______,                                 _______, KC_1,           KC_2,           KC_3,           _______, _______,
+    _______, _______, _______, _______, _______, _______,                                                                                                 _______, KC_0,           _______, _______, _______, _______,
+    _______, KC_NO,          _______,                 _______, _______, KC_ENTER
+  ),
+
+  [SYMB] = LAYOUT_moonlander(
+    _______, _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, KC_SLASH,       _______, _______, _______,
+    _______, KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_HASH,                                        KC_NO,          KC_NO,          _______, _______, _______, KC_ASTR,        _______,
+    TO(0),          KC_PERC,        KC_AMPR,        KC_LPRN,        KC_RPRN,        KC_GRAVE,       _______,                                                                 _______, _______, OSM(MOD_RCTL),  OSM(MOD_RSFT),  OSM(MOD_RALT),  OSM(MOD_RGUI),  KC_KP_MINUS,
+    _______, KC_DLR,         KC_CIRC,        KC_LBRC,        KC_RBRC,        KC_TILD,                                        KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,       KC_MS_RIGHT,    _______, _______,
+    _______, _______, _______, _______, KC_F,           _______,                                                                                                     _______,     KC_MS_BTN1,     KC_MS_BTN2,     KC_NO,          _______, _______,
+    _______, _______, _______,                 _______, _______, RCTL(KC_BSPC)
+  ),
+  [MOVE] = LAYOUT_moonlander(
+    _______, _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, LALT(KC_KP_8),  _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______,
+    TO(0),          _______, _______, _______, _______, _______, _______,                                                                 _______, _______, LGUI(KC_4),     LGUI(KC_5),     LGUI(KC_6),     _______, _______,
+    _______, _______, _______, _______, _______, _______,                                 _______, LGUI(KC_1),     LGUI(KC_2),     LGUI(KC_3),     _______, _______,
+    _______, _______, _______, _______, _______, _______,                                                                                                 _______, _______, _______, _______, _______, _______,
+    _______, _______, _______,                 _______, _______, _______
+  ),
+};
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     if (record->event.pressed) {
+//         switch (keycode) {
+//         case VRSN:
+//             SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (IS_QK_ONE_SHOT_MOD(keycode) && is_oneshot_layer_active() && record->event.pressed) {
+        clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED);
+    }
+    return;
+}
