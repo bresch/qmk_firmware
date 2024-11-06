057a1e27ce673609d214ecc6588dfb4635d2ac4d
bresch <brescianimathieu@gmail.com>
2024-11-06 08:33:26 +0100

    moon: remove mouse

--- keyboards/zsa/moonlander/keymaps/test/keymap.c
+++ keyboards/zsa/moonlander/keymaps/test/keymap.c
@@ -34,12 +34,7 @@
 #define LCL MOD_LCTL
 #define RCL MOD_RCTL
 
-#define MOUSE_L KC_MS_LEFT
-#define MOUSE_R KC_MS_RIGHT
-#define MOUSE_U KC_MS_UP
-#define MOUSE_D KC_MS_DOWN
-#define MOUSE_B1 KC_MS_BTN1
-#define MOUSE_B2 KC_MS_BTN2
+#define KC_COL LCTL(KC_SCLN)
 
 enum layers {
     BASE,
@@ -56,36 +51,36 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
-    _______ , _______ , _______ , KC_QUES , VOL_MUTE, VOL_DOWN, VOL_UP  ,                            _______ , _______ , _______ , KC_EXLM , KC_EQUAL, _______ ,KC_DELETE,
-    _______ , KC_TAB  , KC_L    , KC_Y    , KC_P    , KC_B    , KC_GRAVE,                            _______ , KC_Z    , KC_F    , KC_O    , KC_U    , _______ , _______ ,
-    KC_ESC  , KC_C    , KC_R    , KC_S    , KC_T    , KC_G    , CW_TOGG ,                            KC_MINUS, KC_M    , KC_N    , KC_E    , KC_I    , KC_A    , KC_SCLN ,
-    KC_Q    , KC_W    , KC_J    , KC_V    , KC_D    , KC_K    ,                                                KC_X    , KC_H    , KC_QUOTE, KC_COMMA, KC_DOT  , _______ ,
-    _______ , _______ , _______ , _______ , KC_UNDS ,                     _______ ,         _______ ,                    KC_ASTR , KC_LABK , KC_RABK , _______ , _______ ,
-    KC_SPACE,OSL(NUMB),KC_BSLS  ,                                                                    KC_SLASH,OSL(SYMB), KC_BSPC
+    _______ , _______ , _______ , _______ , _______ , _______ , _______ ,                            _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , KC_ESC  , KC_L    , KC_Y    , KC_P    , KC_B    , _______ ,                            _______ , KC_Z    , KC_F    , KC_O    , KC_U    , _______ , _______ ,
+    _______ , KC_C    , KC_R    , KC_S    , KC_T    , KC_G    , _______ ,                            KC_MINUS, KC_M    , KC_N    , KC_E    , KC_I    , KC_A    , _______ ,
+    _______ , KC_W    , KC_J    , KC_V    , KC_D    , KC_K    ,                                                KC_X    , KC_H    , KC_QUOTE, KC_COMMA, KC_DOT  , _______ ,
+    _______ , _______ , _______ , _______ , KC_UNDS ,                     _______ ,         _______ ,                    KC_ASTR , _______ , _______ , _______ , _______ ,
+                                                      KC_SPACE,OSL(NUMB),KC_BSLS  ,         KC_SLASH,OSL(SYMB), KC_BSPC
   ),
 
   [NUMB] = LAYOUT(
     _______ , _______ , _______ , _______ , _______ , _______ , _______ ,                            _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-    _______ , _______ , _______ ,OSL(ARRO), _______ , _______ , _______ ,                            _______ , _______ , KC_7    , KC_8    , KC_9    , _______ , _______ ,
-    TO(BASE),OSM(LWN),OSM(LAT),OSM(LST),OSM(LCL), _______ , _______ ,                            _______ , _______ , KC_4    , KC_5    , KC_6    , KC_PLUS , _______ ,
-    _______ , _______ , _______ , _______ , _______ , _______ ,                                                _______ , KC_1    , KC_2    , KC_3    , _______ , _______ ,
+    _______ , TO(BASE), _______ ,OSL(ARRO), _______ , _______ , _______ ,                            _______ , _______ , KC_7    , KC_8    , KC_9    , _______ , _______ ,
+    _______ , OSM(LWN), OSM(LAT), OSM(LST), OSM(LCL), _______ , _______ ,                            _______ , KC_MINUS, KC_4    , KC_5    , KC_6    , _______ , _______ ,
+    _______ , _______ , VOL_MUTE, VOL_DOWN, VOL_UP  , _______ ,                                                KW_PLUS , KC_1    , KC_2    , KC_3    , KC_DOT  , _______ ,
     _______ , _______ , _______ , _______ , _______ ,                     _______ ,         _______ ,                    KC_0    , _______ , _______ , _______ , _______ ,
-    _______ , KC_NO   , _______ ,                                                                    _______ , _______ , KC_ENTER
+                                                      _______ , KC_NO   , _______ ,         _______ , _______ ,KC_ENTER
   ),
 
   [SYMB] = LAYOUT(
-    _______ , _______ , _______ , _______ , _______ , _______ , _______ ,                            _______ , _______ , _______ , KC_SLASH, _______ , _______ , _______ ,
-    _______ , KC_EXLM , KC_AT   , KC_LCBR , KC_RCBR , KC_PIPE , KC_HASH ,                            KC_NO   , KC_NO   , _______ , _______ , _______ , KC_ASTR , _______ ,
-    TO(0)   , KC_PERC , KC_AMPR , KC_LPRN , KC_RPRN , KC_GRAVE, _______ ,                            _______ , _______ ,OSM(RCL),OSM(RST),OSM(RAT),OSM(RWN),KC_KP_MINUS,
-    _______ , KC_DLR  , KC_CIRC , KC_LBRC , KC_RBRC , KC_TILD ,                                                MOUSE_L , MOUSE_D , MOUSE_U , MOUSE_R , _______ , _______ ,
-    _______ , _______ , _______ , _______ , KC_F    ,                     _______ ,         _______ ,                    MOUSE_B1, MOUSE_B2, KC_NO   , _______ , _______ ,
-    _______ , _______ , _______ ,                                                                    _______ , _______ , RCTL(KC_BSPC)
+    _______ , _______ , _______ , _______ , _______ , _______ , _______ ,                            _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , TO(BASE), KC_AT   , KC_LCBR , KC_RCBR , KC_PIPE , _______ ,                            KC_NO   , KC_NO   , KC_DLR  , KC_EXLM , KC_EQUAL, KC_ASTR , _______ ,
+    _______ , KC_PERC , KC_AMPR , KC_LPRN , KC_RPRN , KC_GRAVE, _______ ,                            _______ , _______ ,OSM(RCL),  OSM(RST), OSM(RAT), OSM(RWN), _______ ,
+    _______ , KC_Q    , KC_CIRC , KC_LBRC , KC_RBRC , KC_TILD ,                                                _______ , KC_GRAVE, CW_TOGG , KC_SCLN , KC_SCLN , _______ ,
+    _______ , _______ , _______ , _______ , _______ ,                     _______ ,         _______ ,                   KC_DELETE, _______ , _______ , _______ , _______ ,
+                                                      KC_TAB  , _______ , _______ ,         _______ , _______ , _______
   ),
 
   [MOVE] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______,LALT(KC_KP_8),  _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______,
-    TO(0),          _______, _______, _______, _______, _______, _______,                                                                 _______, _______, LGUI(KC_4),     LGUI(KC_5),     LGUI(KC_6),     _______, _______,
+    _______, TO(BASE), _______,LALT(KC_KP_8),  _______, _______, _______,                                 _______, _______, _______, _______, _______, _______, _______,
+    _______,          _______, _______, _______, _______, _______, _______,                                                                 _______, _______, LGUI(KC_4),     LGUI(KC_5),     LGUI(KC_6),     _______, _______,
     _______, _______, _______, _______, _______, _______,                                 _______, LGUI(KC_1),     LGUI(KC_2),     LGUI(KC_3),     _______, _______,
     _______, _______, _______, _______, _______, _______,                                                                                                 _______, _______, _______, _______, _______, _______,
     _______, _______, _______,                 _______, _______, _______
